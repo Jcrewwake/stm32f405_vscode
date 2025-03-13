993d71eda751ecc915a40f83c5db968eac5d28ae
longyingwang <2570715747@qq.com>
2025-03-14 00:31:26 +0800

    移植OLED

--- User/main.c
+++ User/main.c
@@ -1,11 +1,14 @@
 #include "stm32f4xx.h"                  // Device header
 #include "LED.h"
 #include "Delay.h"
+#include "OLED.h"
 
 int main(void)
 {
-    
+    OLED_Init();
     LED_Init();
+    OLED_Printf(0, 0, OLED_6X8, "Hello World!: %lf", 0.918273);
+    OLED_Update();
     while(1) {
         LED_Turn();
         Delay_s(4);
